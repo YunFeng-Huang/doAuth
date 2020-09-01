7f55c4ebe1c5b9d3e998809ad3e05dc9d75a96e6
yunfeng <yunfengjz@qq.com>
2020-09-01 08:50:33 +0800

    first

---
+++ index.js
@@ -0,0 +1,36 @@
+const app = getApp()
+const doAuth = require('./doAuth.js')
+Page({
+  data: {
+
+  },
+  onLoad: function () {
+    wx.login({
+      success(res){
+        console.log(res)
+      },
+      fail(err){
+        console.error(err)
+      }
+    })
+  },
+  doAuth(){
+    doAuth('chooseAddress')
+    // wx.chooseAddress();
+    // wx.getSetting({
+    //   success:function(){
+    //     wx.showModal({
+    //       title: 'title',
+    //       content: 'content',
+    //       cancelText: '取消',
+    //       confirmText: '确定',
+    //       confirmColor: '#21c0ae',
+    //       success: function (res) {
+    //         console.log('efg');
+    //         wx.openSetting({})
+    //       }
+    //     })
+    //   }
+    // })
+  }
+})
